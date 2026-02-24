89c5a7639560e6aba741eb59c99c1c5eb340ffdb
D4vSec <56389697+D4vSec@users.noreply.github.com>
2026-02-24 14:22:50 +0100

    feat(backend): Add support to Postgre

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,8 @@ import app from "#@/app"
 import db from "#config/db"
 const PORT = process.env.API_PORT || 3000
 
-/*db.connect()
+db.connect()
     .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server running on http://localhost:${PORT}`)
-        })
+        app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
     })
     .catch((err) => console.error("DB connection error:", err))
-*/
-
-app.listen(PORT, () => {
-	console.log(`Server running on http://localhost:${PORT}`)
-})
--- backend/src/app.js
+++ backend/src/app.js
@@ -9,15 +9,16 @@ const API_VERSION = "v1"
 app.set("trust proxy", 1)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: "10kb" }))
 app.disable("x-powered-by")
 
 app.use(`/${API_VERSION}`, globalLimiter)
 app.use(`/${API_VERSION}/user`, userRoutes)
 
 app.use((req, res) => {
-	res.status(404).json({
-		error: "Not Found",
-	})
+    res.status(404).json({
+        error: "Not Found",
+    })
 })
 
 export default app
