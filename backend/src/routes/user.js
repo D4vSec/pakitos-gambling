61d8da87ae6c59d623baa848f496e16cf3ea7efa
D4vSec <56389697+D4vSec@users.noreply.github.com>
2026-02-24 14:35:26 +0100

    fear(user): Add routes

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,9 +1,11 @@
 import express from "express"
+import { getProfile, getAllUsers } from "#controllers/userController"
+import authMiddleware from "#middlewares/authMiddleware"
+import adminMiddleware from "#middlewares/adminMiddleware"
+
 const userRoutes = express.Router()
 
-userRoutes.get("/", (req, res) => {
-    // TODO: Bussiness Logic, Controller
-    res.status(200).json({ message: "Hello User!" })
-})
+userRoutes.get("/me", authMiddleware, getProfile)
+userRoutes.get("/", authMiddleware, adminMiddleware, getAllUsers)
 
 export default userRoutes
