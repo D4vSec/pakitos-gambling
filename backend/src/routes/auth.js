9836ccb12d79b5d7e578ffdb1d956f78db942c3f
D4vSec <56389697+D4vSec@users.noreply.github.com>
2026-02-26 17:20:22 +0100

    feat(backend): Auth rate limits

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,10 +1,10 @@
 import express from "express"
 import { register, login } from "#controllers/authController"
-import { registrationLimiter } from "#@/middlewares/rateLimitMiddleware"
+import { authLimiter, registrationLimiter } from "#@/middlewares/rateLimitMiddleware"
 
 const authRoutes = express.Router()
 
 authRoutes.post("/register", registrationLimiter, register)
-authRoutes.post("/login", login)
+authRoutes.post("/login", authLimiter, login)
 
 export default authRoutes
