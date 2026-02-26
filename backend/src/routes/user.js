5ed705d79c6fa186892801840791be26248b5786
D4vSec <56389697+D4vSec@users.noreply.github.com>
2026-02-26 17:38:27 +0100

    fix(backend): auth and user routes base

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,11 +1,17 @@
 import express from "express"
-import { getProfile, getAllUsers } from "#controllers/userController"
+import { getProfile, getAllUsers, deleteSelf, updateSelf } from "#controllers/userController"
 import authMiddleware from "#middlewares/authMiddleware"
 import adminMiddleware from "#middlewares/adminMiddleware"
 
 const userRoutes = express.Router()
 
 userRoutes.get("/me", authMiddleware, getProfile)
+userRoutes.put("/me", authMiddleware, updateSelf)
+userRoutes.delete("/me", authMiddleware, deleteSelf)
+
 userRoutes.get("/", authMiddleware, adminMiddleware, getAllUsers)
+userRoutes.get("/:id", authMiddleware, adminMiddleware)
+userRoutes.put("/:id", authMiddleware, adminMiddleware)
+userRoutes.delete("/:id", authMiddleware, adminMiddleware)
 
 export default userRoutes
