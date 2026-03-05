a3020bb8368c8cbecc1da8c81f3c72dfae126132
Stevankito <nainpontes2016@gmail.com>
2026-03-05 18:52:22 +0100

    feat(session): Get user data an show name and balance

--- frontend/src/hooks/useAPI.js
+++ frontend/src/hooks/useAPI.js
@@ -41,7 +41,7 @@ const useAPI = () => {
         }
     }
 
-    const get = async (url) => await request(url, { method: "GET" })
+    const get = async (url, options = {}) => await request(url, { method: "GET", ...options })
     const post = async (url, body) => await request(url, { method: "POST", body: body })
     const put = async (url, body) => await request(url, { method: "PUT", body: body })
     const patch = async (url, body) => await request(url, { method: "PATCH", body: body })
--- frontend/src/providers/SessionProvider.jsx
+++ frontend/src/providers/SessionProvider.jsx
@@ -6,17 +6,35 @@ import { useLocale } from "./LocaleProvider"
 const SessionContext = createContext()
 
 const SessionProvider = ({ children }) => {
-    const [accessToken, setAccessToken] = useState(null)
-    const [refreshToken, setRefreshToken] = useState(null)
-    const [user, setUser] = useState(null)
+    const defaultUser = {}
+    const defaultAccessToken = ""
+
+    const [user, setUser] = useState(defaultUser)
+    const [accessToken, setAccessToken] = useState(defaultAccessToken)
+    // Refresh token in localStorage
     const [isLogged, setIsLogged] = useState(false)
     const [loading, setLoading] = useState(true)
 
     const { addNotification } = useNotification()
     const { t } = useLocale()
-    const { post } = useAPI()
+
+    const { get, post, put, patch, destroy } = useAPI()
 
     const navigate = useNavigate()
+
+    const setRefreshToken = (refreshToken) => {
+        localStorage.setItem("refreshToken", refreshToken)
+        return refreshToken
+    }
+
+    const getRefreshToken = () => {
+        const refreshToken = localStorage.getItem("refreshToken")
+        return refreshToken || null
+    }
+
+    const removeRefreshToken = () => {
+        localStorage.removeItem("refreshToken")
+    }
 
     const register = async (data) => {
         try {
@@ -24,35 +42,94 @@ const SessionProvider = ({ children }) => {
             console.log(response)
 
             if (response?.code !== "AUTH_USER_REGISTERED") {
-                throw new Error(t(`message.error.${response?.code}`))
+                throw new Error(response?.code)
             }
 
             addNotification(t(`message.success.${response?.code}`), "success")
             addNotification(t("message.info.registered"), "info", 7000)
             navigate("/login")
         } catch (error) {
-            addNotification(t(`message.error.${error?.message}`) || error?.message, "error")
+            addNotification(error?.message, "error")
         }
     }
 
     const login = async (data) => {
         try {
             const response = await post("/api/v1/auth/login", data)
-            console.log("ae", response)
+            console.log("res", response)
 
-            // TODO: Cambiar cuando del david me ponga un code con el token
             if (response.code) {
-                throw new Error(t(`message.error.${response?.code}`))
+                throw new Error(response?.code)
             }
 
-            addNotification(t(`message.success.${"AUTH_USER_LOGGED"}`), "success")
+            const { accessToken, refreshToken } = response
+
+            setAccessToken(accessToken)
+            setRefreshToken(refreshToken)
+            setIsLogged(true)
+
+            addNotification(t(`message.success.AUTH_USER_LOGGED`), "success")
+
+            const user = await getUserData()
+            console.log("user", user)
+            setUser(user)
+
+            navigate("/home")
         } catch (error) {
-            addNotification(t(`message.error.${error?.message}`) || error?.message, "error")
+            addNotification(error?.message, "error")
         }
     }
 
-    const value = { register, login }
+    const logout = () => {
+        removeRefreshToken()
+        setAccessToken(defaultAccessToken)
+        setUser(defaultUser)
+        setIsLogged(false)
+        addNotification(t("message.success.logout"), "success")
+        navigate("/")
+    }
 
+    const getUserData = async () => {
+        // TODO: Se pierde el accToken al F5, hay veces que me indica
+        // unauthorized
+        try {
+            console.log("acc", accessToken)
+            const response = await get("/api/v1/user/me", {
+                headers: { Authorization: `Bearer ${accessToken}` },
+            })
+            console.log("rr", response)
+            if (response.code) {
+                throw new Error(response?.code)
+            }
+
+            setUser(response)
+            setIsLogged(true)
+        } catch (error) {
+            addNotification(error?.message, "error")
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        const storedRefreshToken = getRefreshToken()
+
+        if (storedRefreshToken) {
+            setRefreshToken(storedRefreshToken)
+        }
+
+        setLoading(false)
+    }, [])
+
+    const value = {
+        register,
+        login,
+        logout,
+        user,
+        accessToken,
+        isLogged,
+        loading,
+    }
     return <SessionContext value={value}>{children}</SessionContext>
 }
 
