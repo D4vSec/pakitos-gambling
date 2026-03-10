3606b50ad90b2d22c96079b9b2bc0efe163ec403
Stevankito <nainpontes2016@gmail.com>
2026-03-10 16:40:09 +0100

    refactor(roulette): Refactor roulette structure

--- frontend/src/components/games/roulette/Roulette0.jsx
+++ frontend/src/components/games/roulette/Roulette0.jsx
@@ -1,15 +1,16 @@
 import React from "react"
 import RouletteTable from "./RouletteTable"
 import rouletteImg from "@/assets/roulette0.png"
+
 const Roulette0 = () => {
     return (
-        <div className="w-full h-full grid grid-rows-[60%_40%] grid-cols-[1fr_2fr_1fr] gap-2">
+        <div className="w-full h-full grid grid-rows-[55%_4 5%] grid-cols-[1fr_2fr_1fr] gap-2">
             <div className="flex justify-center items-center">
                 <div className="bg-base-300 w-20 h-20 rounded-lg">last num</div>
             </div>
 
             <div className="flex justify-center items-center">
-                <img src={rouletteImg} alt="Roulette0" className="max-w-full max-h-full" />
+                <img src={rouletteImg} alt="Roulette0" className="max-w-9       0"/>
             </div>
 
             <div className="flex flex-col justify-center items-center gap-2">
