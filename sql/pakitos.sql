c1380a61b92fcdf7b31ae5b77692a6c229986905
D4vSec <56389697+D4vSec@users.noreply.github.com>
2026-03-16 15:52:51 +0100

    feat(sql): add base tables

--- sql/pakitos.sql
+++ sql/pakitos.sql
@@ -3,6 +3,9 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 DROP TABLE IF EXISTS sessions;
 DROP TABLE IF EXISTS transactions;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS bets;
+DROP TABLE IF EXISTS bets_info;
+DROP TABLE IF EXISTS user_bets;
 
 CREATE TYPE user_role AS ENUM ('user', 'admin');
 CREATE TYPE transaction_type AS ENUM ('deposit', 'withdrawal');
@@ -34,4 +37,30 @@ CREATE TABLE transactions (
     type transaction_type NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE bets (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ends_at TIMESTAMP NOT NULL,
+    label VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE bets_info (
+    bet_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    label VARCHAR(255) NOT NULL,
+    odd DECIMAL(5, 2) NOT NULL CHECK (odd > 1.00),
+    FOREIGN KEY (bet_id) REFERENCES bets(id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_bets (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL,
+    bet_id UUID NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL CHECK (amount > 0),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (bet_id) REFERENCES bets(id) ON DELETE CASCADE
 );
