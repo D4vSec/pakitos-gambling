8911069ebdef1f758361bb3851d8b81efbcc4acb
yeraox <yeraoxcaturlax@gmail.com>
2026-03-16 16:57:23 +0100

    fix(bets): fix import missing

--- backend/src/routes/bets.js
+++ backend/src/routes/bets.js
@@ -1,5 +1,11 @@
 import express from "express"
-import { getBets, getBetInfo, deleteBet, updateBet } from "#controllers/betsController"
+import {
+    getBets,
+    getBetInfo,
+    deleteBet,
+    updateBet,
+    placeBet,
+} from "#controllers/betsController"
 import authMiddleware from "#middlewares/authMiddleware"
 import adminMiddleware from "#middlewares/adminMiddleware"
 import { gameLimiter } from "#middlewares/rateLimitMiddleware"
@@ -8,8 +14,20 @@ const betsRoutes = express.Router()
 
 betsRoutes.get("/", authMiddleware, gameLimiter, getBets)
 betsRoutes.get("/:betId", authMiddleware, gameLimiter, getBetInfo)
-betsRoutes.delete("/:betId", authMiddleware, adminMiddleware, gameLimiter, deleteBet)
-betsRoutes.put("/:betId", authMiddleware, adminMiddleware, gameLimiter, updateBet)
+betsRoutes.delete(
+    "/:betId",
+    authMiddleware,
+    adminMiddleware,
+    gameLimiter,
+    deleteBet,
+)
+betsRoutes.put(
+    "/:betId",
+    authMiddleware,
+    adminMiddleware,
+    gameLimiter,
+    updateBet,
+)
 
 betsRoutes.post("/:betId/place", authMiddleware, gameLimiter, placeBet)
 
---
+++ backend/src/services/capyroad.js
@@ -0,0 +1,12 @@
+import { randomInt } from "#utils/rng"
+
+const createCapyRoad = () => {
+    createGame = () => {
+        const crash = 0
+        const multiplier = 1.0
+        const isCrashed = false
+    }
+    return {}
+}
+
+export default createCapyRoad
