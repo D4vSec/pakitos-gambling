0e682cde5f1c4a31f50fe45f915cee1bd6c303cf
Stevankito <nainpontes2016@gmail.com>
2026-04-23 10:41:34 +0200

    fix(notifications): Fix modal notifications not showing correctly

--- frontend/src/components/notification/NotificationModal.jsx
+++ frontend/src/components/notification/NotificationModal.jsx
@@ -3,40 +3,46 @@ import { useNotification } from "@/providers/NotificationProvider.jsx"
 import Button from "@/components/buttons/Button.jsx"
 
 const NotificationModal = ({ notification }) => {
-    const { removeNotification } = useNotification()
-    const { id, message, type, options } = notification
-    const [open, setOpen] = useState(true)
+  const { removeNotification } = useNotification()
+  const { id, message, type, options } = notification
+  const [open, setOpen] = useState(true)
 
-    const accept = () => {
-        options?.onAccept?.()
-        removeNotification(id)
-    }
+  const accept = () => {
+    options?.onAccept?.()
+    setOpen(false)
+    removeNotification(id)
+  }
 
-    const cancel = () => {
-        setOpen(false)
-        setTimeout(() => {
-            removeNotification(id)
-        }, 500)
-    }
+  const cancel = () => {
+    setOpen(false)
+    setTimeout(() => {
+      removeNotification(id)
+    }, 500)
+  }
 
-    return (
-        <dialog
-            id="my_modal_5"
-            className={`modal ${open === true ? "modal-open" : "modal-close"} modal-bottom sm:modal-middle`}
-        >
-            <div className="modal-box">
-                <h3 className="font-bold text-lg text-center mb-6">{message}</h3>
-                <div className="flex flex-col sm:flex-row justify-center gap-4 ">
-                    <Button variant="error" className="px-8 py-4 sm:flex-1" onClick={accept}>
-                        {options?.acceptLabel || "Yes"}
-                    </Button>
-                    <Button variant="neutral" className="px-8 py-4 sm:flex-1" onClick={cancel}>
-                        {options?.cancelLabel || "No"}
-                    </Button>
-                </div>
-            </div>
-        </dialog>
-    )
+  return (
+    <dialog
+      id="my_modal_5"
+      className={`modal ${open === true ? "modal-open" : "modal-close"} modal-bottom sm:modal-middle`}>
+      <div className="modal-box">
+        <h3 className="font-bold text-lg text-center mb-6">{message}</h3>
+        <div className="flex flex-col sm:flex-row justify-center gap-4 ">
+          <Button
+            variant="error"
+            className="px-8 py-4 sm:flex-1"
+            onClick={accept}>
+            {options?.acceptLabel || "Yes"}
+          </Button>
+          <Button
+            variant="neutral"
+            className="px-8 py-4 sm:flex-1"
+            onClick={cancel}>
+            {options?.cancelLabel || "No"}
+          </Button>
+        </div>
+      </div>
+    </dialog>
+  )
 }
 
 export default NotificationModal
--- frontend/src/components/notification/Notifications.jsx
+++ frontend/src/components/notification/Notifications.jsx
@@ -11,7 +11,7 @@ const Notifications = () => {
   if (global.length === 0) return null
 
   return (
-    <div className="fixed top-5 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2 z-9999">
+    <div className="pointer-events-none fixed top-5 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2 z-9999 w-screen h-screen">
       {global.map((notification) => {
         switch (notification?.type) {
           case "modal":
