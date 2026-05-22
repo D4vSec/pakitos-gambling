1ef68995efe440c075557680f71f4ecf1c111610
D4vSec <56389697+D4vSec@users.noreply.github.com>
2026-05-22 12:21:12 +0200

    chore: update compose for handling api port

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,12 @@ const PORT = process.env.API_PORT || 3000
 
 db.connect()
     .then(() => {
-        app.listen(PORT, () => {
+        app.listen(PORT, '0.0.0.0', () => {
             logger.info(`Server running on http://localhost:${PORT}`)
 
             if (process.env.NODE_ENV === "development")
                 logger.warn("Running in development mode. Don't use this in production!")
-            
+
         })
     })
     .catch((err) => logger.fatal("DB connection error:", err))
