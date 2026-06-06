5fee5f0c2655f4f4f2cccbd6a71c49550822336f
Stevankito <nainpontes2016@gmail.com>
2026-06-06 14:02:42 +0200

    refactor(frontend): remove unused components

--- frontend/src/components/games/roulette/board/InnerBet.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-// TODO: hacer apuestas entre numeros
-const InnerBet = () => {
-  return <div></div>
-}
-
-export default InnerBet
--- frontend/src/providers/LocalStorageProvider.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const LocalStorageProvider = () => {
-    return <div>LocalStorageProvider</div>
-}
-
-export default LocalStorageProvider
